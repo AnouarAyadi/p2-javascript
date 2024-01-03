507a2faa8536127e081b260e0b795bbffba17ecf
AnouarAyadi <9018933@student.zadkine.nl>
2024-01-03 19:39:04 +0100

    opdracht3

---
+++ opdracht_3/app.js
@@ -0,0 +1,9 @@
+let output = document.getElementById("title");
+
+for(let i = 0; i< 101; i++ ){
+    output.innerHTML += i + "<br>" ;
+}
+
+for(let o = 100; o >= 0; o-- ){
+    output.innerHTML += o + "<br>" ;
+}
